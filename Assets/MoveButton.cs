3fcec3ae92738b9450907eee9f04dba4628541fd
ipowerilya <idestinyilya@gmail.com>
2020-01-25 13:26:34 +0300

    commit

---
+++ Assets/MoveButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class MoveButton : MonoBehaviour
+{
+
+    RectTransform canvas;
+    RectTransform button;
+    Vector3 startingPosition;
+    public float speed;
+    public bool unzippnig = false;
+    public MainRuntimeBehaviour unz;
+   
+
+    void Start()
+    {
+        button = gameObject.GetComponent<RectTransform>();
+        canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        startingPosition = transform.position;
+        speed = -5f;
+        unz = GameObject.Find("ARCamera").GetComponent<MainRuntimeBehaviour>();
+        
+   
+    }
+
+    void Update()
+    {
+        unzippnig = unz.doThing;
+        if (unzippnig)
+        {
+            transform.Translate(0f, speed, 0f);
+            if (button.position.y < -button.rect.height)
+                transform.position = new Vector3(startingPosition.x, canvas.rect.height + button.rect.height, startingPosition.z);
+        }
+    }
+}
---
+++ Assets/firstLaunch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class firstLaunch : MonoBehaviour
+{
+  
+
+    // Use this for initialization
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("FirstLaunch") == 0)
+        {
+            //First launch
+            PlayerPrefs.SetInt("FirstLaunch", 1);
+            SceneManager.LoadScene("HowTo");
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            //Load scene_02 if its not the first launch
+            SceneManager.LoadScene("AR");
+        }
+    }
+
+}
+
